6148daede0263d77fd0ed46b1cd37a0679cc1ba1
MaryannGitonga <naygitonga4@gmail.com>
2024-09-07 13:46:09 +0300

    Add scheduler logic

--- cpu/src/vcpu_scheduler.c
+++ cpu/src/vcpu_scheduler.c
@@ -64,7 +64,93 @@ int main(int argc, char *argv[])
 /* COMPLETE THE IMPLEMENTATION */
 void CPUScheduler(virConnectPtr conn, int interval)
 {
+	virDomainPtr *domains;
+	int ndomains, npcpuParams, result, nvcpus, npcpus;
+	virNodeCPUStatsPtr pcpuParams;
+	unsigned char *cpuMaps;
 
+	// collect host pcpu stats
+	npcpus = virNodeGetCPUMap(conn, NULL, NULL, 0);
+	if (npcpus < 0) {
+        fprintf(stderr, "Failed to get npcpus\n");
+        return;
+    }
+
+	if (virNodeGetCPUStats(conn, -1, NULL, &npcpuParams, 0) < 0)
+	{
+		fprintf(stderr, "Failed to get nparams for pcpu stats\n");
+        return;
+	}
+
+	pcpuParams = calloc(npcpuParams, sizeof(virNodeCPUStats));
+	result = virNodeGetCPUStats(conn, -1, pcpuParams, &npcpuParams, 0);
+	if (result < 0) {
+        fprintf(stderr, "Failed to get pcpu stats\n");
+        free(pcpuParams);
+        return;
+    }
+
+	// get all active running VMs
+	ndomains = virConnectListAllDomains(conn, &domains, VIR_CONNECT_LIST_DOMAINS_RUNNING);
+
+	if(ndomains < 0){
+		fprintf(stderr, "Failed to get active running VMs\n");
+		free(pcpuParams);
+		return;
+	}
+	
+	// collect vcpu stats
+	for (int i = 0; i < ndomains; i++)
+	{
+		// get the number of vcpus in the domain
+        nvcpus = virDomainGetMaxVcpus(domains[i]);
+        if (nvcpus < 0) {
+            fprintf(stderr, "Failed to get nvcpus for domain %d\n", i);
+            continue;
+        }
+
+        // cpuMaps = malloc(nvcpus * VIR_CPU_MAPLEN(npcpus));
+
+		// Single vcpu or multiple? (above is multiple)
+		cpuMaps = malloc(VIR_CPU_MAPLEN(npcpus));
+        if (cpuMaps == NULL) {
+            fprintf(stderr, "Failed to allocate memory for CPU maps\n");
+            continue;
+        }
+        memset(cpuMaps, 0, VIR_CPU_MAPLEN(npcpus));
+
+		// determine best pcpu
+		for (int j = 0; j < nvcpus; j++)
+		{
+			int bestpcpu = -1;
+			long long minLoad = LLONG_MAX;
+
+			for (int k = 0; k < npcpuParams; k++)
+			{
+				if (pcpuParams[k].cpu_time < minLoad)
+				{
+					minLoad = pcpuParams[k].cpu_time;
+					bestpcpu = k;
+				}
+			}
+
+			if (bestpcpu != -1)
+			{
+				cpuMaps[bestpcpu / 8] |= (1 << (bestpcpu % 8));
+
+				result = virDomainPinVcpu(domains[i], j, cpuMaps, VIR_CPU_MAPLEN(npcpus));
+				if (result < 0)
+				{
+					fprintf(stderr, "Failed to pin vcpu %d to pcpu %d for domain %d\n", j, bestpcpu, i);
+				}
+			}
+		}
+
+		free(cpuMaps);
+	}
+
+	free(pcpuParams);
+	free(domains);
 }
 
 
