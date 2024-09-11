29f2a16586fcbb1ee350b92ae7058126749a14d5
MaryannGitonga <naygitonga4@gmail.com>
2024-09-11 10:09:20 +0300

    Add print statements

--- cpu/src/vcpu_scheduler.c
+++ cpu/src/vcpu_scheduler.c
@@ -61,23 +61,6 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-/* calculate standard deviation of pCPU loads */
-double calculateStandardDev(long long *pcpuLoads, int npcpus) {
-    long long sum = 0;
-    for (int i = 0; i < npcpus; i++) {
-        sum += pcpuLoads[i];
-    }
-    double mean = sum / (double)npcpus;
-
-    double variance = 0.0;
-    for (int i = 0; i < npcpus; i++) {
-        variance += pow(pcpuLoads[i] - mean, 2);
-    }
-    variance /= npcpus;
-    
-    return sqrt(variance);
-}
-
 /* COMPLETE THE IMPLEMENTATION */
 void CPUScheduler(virConnectPtr conn, int interval)
 {
@@ -147,43 +130,39 @@ void CPUScheduler(virConnectPtr conn, int interval)
                         pcpuLoads[k] += vcpuTime;
                     }
 				}
-				printf("  CPU time for domain %d: %llu\n", i, vcpuTime);
+				// printf("  CPU time for domain %d: %llu\n", i, vcpuTime);
 			}
         }
 
 		free(params);
 		free(cpuMaps);
 
-		// Calculate standard deviation
-		double stddev = calculateStandardDev(pcpuLoads, npcpus);
-		printf("Standard deviation of pCPU loads: %.2f\n", stddev);
-
-		if (stddev >= 5.0) {
-			printf("System is unbalanced. Adjusting pinning...\n");
-
-			// 6. find "best" pcpu to pin vcpu
-			int bestPCPU = 0;
-			long long minLoad = pcpuLoads[0];
-			for (int k = 1; k < npcpus; k++) {
-				// try print out each pcpuLoad and min load... and the best cpu picked
-				if (pcpuLoads[k] < minLoad) {
-					bestPCPU = k;
-					minLoad = pcpuLoads[k];
-				}
+		// 6. find "best" pcpu to pin vcpu
+		int bestPCPU = 0;
+		long long minLoad = pcpuLoads[0];
+		for (int k = 1; k < npcpus; k++) {
+			printf("Load %llu is in pcpu %d", pcpuLoads[k], k);
+			// try print out each pcpuLoad and min load... and the best cpu picked
+			if (pcpuLoads[k] < minLoad) {
+				bestPCPU = k;
+				minLoad = pcpuLoads[k];
 			}
-
-			// 7. change pcpu assigned to vcpu
-			unsigned char *bestPCPUMap = calloc(VIR_CPU_MAPLEN(npcpus), sizeof(unsigned char));
-			memset(bestPCPUMap, 0, VIR_CPU_MAPLEN(npcpus));
-			bestPCPUMap[bestPCPU / 8] |= (1 << (bestPCPU % 8));
-
-			result = virDomainPinVcpu(domain, 0, bestPCPUMap, VIR_CPU_MAPLEN(npcpus));
-			if (result < 0) {
-				fprintf(stderr, "Failed to pin vcpu to pcpu %d for domain %d\n", bestPCPU, i);
-			}
-
-			free(bestPCPUMap);
 		}
+
+		printf("Min load %llu is in pcpu %d", minLoad, bestPCPU);
+
+		// 7. change pcpu assigned to vcpu
+		unsigned char *bestPCPUMap = calloc(VIR_CPU_MAPLEN(npcpus), sizeof(unsigned char));
+		memset(bestPCPUMap, 0, VIR_CPU_MAPLEN(npcpus));
+		bestPCPUMap[bestPCPU / 8] |= (1 << (bestPCPU % 8));
+
+		result = virDomainPinVcpu(domain, 0, bestPCPUMap, VIR_CPU_MAPLEN(npcpus));
+		if (result < 0) {
+			fprintf(stderr, "Failed to pin vcpu to pcpu %d for domain %d\n", bestPCPU, i);
+		}
+
+		pcpuLoads[bestPCPU] += params[j].value.ul;
+		free(bestPCPUMap);
 	}
 
 	free(pcpuLoads);
