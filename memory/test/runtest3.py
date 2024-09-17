6ea7637c3d7fbbb3acc584d4db2bd0ae05cb254e
MaryannGitonga <naygitonga4@gmail.com>
2024-09-17 09:44:36 +0300

    Fix test cases

--- memory/test/runtest3.py
+++ memory/test/runtest3.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
         elif tmp == 1:
             ipsAndVals[ip]="B"
         tmp+=1
-    TestLib.startTestCase("~/testcases/3/run {}",ipsAndVals)
+    TestLib.startTestCase("~/AOS_Project1/memory/test/testcases/3/run {}",ipsAndVals)
 
         
 
