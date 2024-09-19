53cf82cdfabfede67a2e43d1f70cf81075e5392f
MaryannGitonga <naygitonga4@gmail.com>
2024-09-19 10:13:28 +0300

    Add break line

--- cpu/src/vcpu_scheduler.c
+++ cpu/src/vcpu_scheduler.c
@@ -133,7 +133,7 @@ void CPUScheduler(virConnectPtr conn, int interval)
 			if (strcmp(params[j].field, "vcpu_time") == 0) {
 				double vcpuTimeInSeconds = params[j].value.ul / pow(10, 9);
 				// double usage = (vcpuTimeInSeconds - prevVcpuTimes[i])/interval;
-				printf("aos_vm_%d time: %.2f", i + 1, vcpuTimeInSeconds);
+				printf("aos_vm_%d time: %.2f\n", i + 1, vcpuTimeInSeconds);
 				double usage = (vcpuTimeInSeconds)/interval;
 				vcpuUsage[i] = usage;
 				for (int k = 0; k < npcpus; k++) {
